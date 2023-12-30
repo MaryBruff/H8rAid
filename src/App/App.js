029c0c27ed0e6c87833c26c7d739cf82a78c3d5c
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-30 15:28:31 -0500

    Main menu routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WikipediaSearch from '../Search/Search';
-import "./App.css"
+import { Route, Routes, Navigate } from 'react-router-dom';
+import './App.css';
 
 function App() {
+  // const [initialLoad, setInitialLoad] = useState(false);
+
+  // useEffect(() => {
+  //   const pageLoad = () => {
+  //     setInitialLoad(true);
+  //   };
+
+  //   window.addEventListener('load', pageLoad);
+
+  //   return () => {
+  //     window.removeEventListener('load', pageLoad);
+  //   };
+  // }, []);
+
+  // if (!initialLoad) {
+  //   return <div>Loading...</div>;
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='header-text'>H8rAid!</h1>
         <button className='login-button'>Login</button>
       </header>
-        <WikipediaSearch />
+      <Routes>
+        <Route path='/' element={<Navigate to='/main' />} />
+        <Route path='/main' element={<WikipediaSearch />} />
+      </Routes>
     </div>
   );
 }
