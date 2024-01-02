1335796aa476fbe5c08942daa4553b52629e8fd0
Arden Ranta <arranta20@gmail.com>
2024-01-02 15:33:16 -0800

    feat/add auth0 setup for most recent react version

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
         domain="dev-nmwu6caxqykltiqw.us.auth0.com"
         clientId="vr4CpEnTxEiwRlO4UhrlT3HJ5Ko2Qd3i"
         authorizationParams={{
-          redirect_uri: "http://localhost:3000"
+          redirect_uri: "http://localhost:3000/"
         }}
       >
         <App />
@@ -25,7 +25,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
