345e4da592a5d94ed62de67871e6b608a52e52a6
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-06 16:10:04 -0500

    Tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,23 @@ describe('template spec', () => {
   })
 })
 
+//Need to login before testing
 describe('API Tests', () => {
   it('Fetches and displays Wikipedia page contents', () => {
-    cy.intercept('GET', 'https://en.wikipedia.org/w/api.php?').as('fetchContents');
-    cy.visit('/'); 
+    cy.intercept('GET', 'https://en.wikipedia.org/w/api.php?', {
+      statusCode: 200,
+      fixture: 'wikipediaApiResponse.json'
+
+    }).as('fetchContents');
+    cy.visit('http://localhost:3000/');
     cy.wait('@fetchContents').then(({ response }) => {
       expect(response.statusCode).to.equal(200);
     });
+
     cy.contains('Loading...').should('not.exist');
-    cy.contains('An error occurred').should('not.exist'); 
+    cy.contains('An error occurred').should('not.exist');
   });
+
 
   it('Handles error when API fails', () => {
     cy.intercept('GET', 'https://en.wikipedia.org/w/api.php?', {
@@ -21,7 +28,7 @@ describe('API Tests', () => {
       body: 'Server Error',
       delayMs: 200,
     }).as('fetchError');
-    cy.visit('/'); 
+    cy.visit('http://localhost:3000/'); 
     cy.wait('@fetchError').then(({ response }) => {
       expect(response.statusCode).to.equal(500);
     cy.contains('An error occurred').should('exist');
@@ -31,13 +38,13 @@ describe('API Tests', () => {
 
 describe('App Component', () => {
   it('Loads the app properly', () => {
-    cy.visit('/'); 
+    cy.visit('http://localhost:3000/'); 
     cy.get('.header-text').should('be.visible');
     cy.get('.login-button').should('be.visible');
   });
 
   it('Displays footer controversies', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
     cy.get('.footer-card').should('be.visible');
     cy.get('.footer-text').should('contain', 'Random Controversy');
   });
@@ -45,33 +52,33 @@ describe('App Component', () => {
 
 describe('Can search for a Controversy', () => {
   it('Searches for a term', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
     cy.get('input[type="text"]').type('SearchTerm{enter}');
   });
 
   it('Displays controversies for a search result', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
   });
 });
 
 describe('Card Component', () => {
   it('Renders snippet properly', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
     cy.get('.card-content').should('be.visible');
   });
 
   it('Handles show more/show less functionality', () => {
-    cy.visit('/');
+    cy.visit('http://localhost:3000/');
   });
 });
 
 describe('WikipediaPage Component', () => {
   it('Loads page content properly', () => {
-    cy.visit('/article/PageTitle'); 
+    cy.visit('http://localhost:3000/Profile'); 
     cy.get('.article').should('be.visible');
   });
 
   it('Saves controversy properly', () => {
-    cy.visit('/'); 
+    cy.visit('http://localhost:3000/'); 
   });
 });
